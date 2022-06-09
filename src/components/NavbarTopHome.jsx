d88eedd9789a1ba7a7fd08f59fea23cbf0879104
Kevin Brian Briceno <72961950+7-18@users.noreply.github.com>
2022-06-09 18:35:47 -0500

    Update NavbarTopHome.jsx

--- src/components/NavbarTopHome.jsx
+++ src/components/NavbarTopHome.jsx
@@ -1,29 +1,34 @@
-import logo from "../assets/images/LOGOLOGO3.png";
-import { AiOutlineHeart } from "react-icons/ai";
-import { TbMessages } from "react-icons/tb";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import CottageIcon from "@mui/icons-material/Cottage";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AddIcon from "@mui/icons-material/Add";
+import { NavLink } from "react-router-dom";
 
-export const NavbarTopHome = () => {
+export const NavbarBottom = () => {
+  const [value, setValue] = React.useState(0);
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignContent: "center",
-        padding: "5px 10px",
-      }}
-    >
-      <img src={logo} alt="logo" />
-      <div
-        style={{
-          fontSize: "25px",
-          display: "flex",
-          gap: "20px",
-          paddingTop: "5px",
-        }}
-      >
-        <AiOutlineHeart />
-        <TbMessages />
-      </div>
-    </div>
+    <Box style={{ width: "100%" }}>
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}>
+        <BottomNavigationAction
+          icon={
+            <NavLink to="/home" style={{ color: "var(--white)" }}>
+              <CottageIcon />
+            </NavLink>
+          }/>
+        <BottomNavigationAction icon={<SearchIcon />} />
+        <BottomNavigationAction icon={<AddIcon />} />
+        <BottomNavigationAction icon={<NotificationsIcon />} />
+      </BottomNavigation>
+    </Box>
   );
 };
