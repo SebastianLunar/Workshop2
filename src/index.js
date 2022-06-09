72cba7e40dd62fdc513186793be5d7c24104a89a
Kevin Brian Briceno <72961950+7-18@users.noreply.github.com>
2022-06-09 18:07:25 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { AppRoute } from './Routes/AppRoute';
+import AppRoute from './Routes/AppRoute';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
