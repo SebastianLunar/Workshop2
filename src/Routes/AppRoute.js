9e40320d8917fb1d101b0e006a83cac3e8b61cc9
7-18 <kevinbrianbr@gmail.com>
2022-06-09 18:08:36 -0500

    routes

--- src/Routes/AppRoute.js
+++ src/Routes/AppRoute.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from '../containers/Login';
-import Register from '../containers/Register';
-import Detail from '../components/Detail';
-import { Home } from '../containers/Home';
-import Perfil from '../containers/Perfil';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../containers/Login";
+import Register from "../containers/Register";
+import Detail from "../components/Detail";
+import { Home } from "../containers/Home";
 
-const AppRoute = () => {
-    return (
-        <div>
-            <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login/>}/>
-                <Route path="/home" element={<Home />}/>
-                <Route path="/register" element={<Register/>}/>
-                <Route path="/detail/:id" element={<Detail />} />
-                <Route path="/perfil" element={<Perfil/>}/>
-            </Routes>
-            </BrowserRouter>
-        </div>
-    )
-}
-
-
-
-    export default AppRoute;
+export const AppRoute = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
